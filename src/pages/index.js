2014a9927dea4d48e157b24b52efce3d429e8316
Paul Kim <Paul.Eun.Kim@gmail.com>
2018-07-03 12:47:39 -0600

    typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ const IndexPage = () => (
         </div>
         <div className={styles.paragraphHeader}>첫번재 - STOP LOSS 세팅 방법</div>
         <div className={styles.paragraph}>
-          이때까지 외환 투자나 선물투자에서 손실을 제한하고자 스탑 오더를 넣었지만 스탑만 치고 다시 올라가는 일들이 허다하기 때문에 스탑 오더를 사용하는 것보다 스탑 오더와 비슷한 역할을 하는 put or buy option을 사는 것입니다. 많은 분이 옵션이라고 하면 위험한 투자를 생각하는데 그것은 공매도(short selling) 때문에 그런 부정적인 인식이 있는 것이고 put이나 call을 사는 option 투자는 처음 내는 premium이라는 금액이 RISK의 전부인 투자 기법입니다. 외환 투자에는 옵션이 없습니다, 그래서 외환투자를하시는분이 효과적으로 트레이딩을 하시려면 선물거래구좌도 함께 가지고 계시면서 필요할 때에 선물거래구좌에서 currency 선물 마켓에서 해당 currency 옵션을 사면서 그것으로 stop order를 대신하는것입니다.        </div>
+          이때까지 외환 투자나 선물투자에서 손실을 제한하고자 스탑 오더를 넣었지만 스탑만 치고 다시 올라가는 일들이 허다하기 때문에 스탑 오더를 사용하는 것보다 스탑 오더와 비슷한 역할을 하는 put or call option을 사는 것입니다. 많은 분이 옵션이라고 하면 위험한 투자를 생각하는데 그것은 공매도(short selling) 때문에 그런 부정적인 인식이 있는 것이고 put이나 call을 사는 option 투자는 처음 내는 premium이라는 금액이 RISK의 전부인 투자 기법입니다. 외환 투자에는 옵션이 없습니다, 그래서 외환투자를하시는분이 효과적으로 트레이딩을 하시려면 선물거래구좌도 함께 가지고 계시면서 필요할 때에 선물거래구좌에서 currency 선물 마켓에서 해당 currency 옵션을 사면서 그것으로 stop order를 대신하는것입니다.        </div>
         <div className={styles.paragraph}>
           이것은 유로달러 옵션으로 스탑 오더를 대신 해서 리스크를 줄이고 플레이를 할 수 있는 방법입니다.
         </div>
